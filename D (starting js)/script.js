3065ceb2573320c073cbc84e717caec65e9694ef
Ranger <mnipk1243@gmail.com>
2025-07-29 00:55:44 +0500

    prototypal inheritance

--- D (starting js)/script.js
+++ D (starting js)/script.js
@@ -3,6 +3,7 @@
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
+  
   this.sayName = function() {
     console.log(this.name)
   };
@@ -21,3 +22,26 @@ Player.prototype.sayHi=function(){
   console.log("Hi")
 }
 
+
+// Don't do this!
+console.log(player1.__proto__ === Player.prototype); // returns true
+console.log(player2.__proto__ === Player.prototype); // returns true
+
+function person(name, age, location){
+  this.name = name;
+  this.age = age;
+  this.location = location;
+}
+let mustafa = new person("Mustafa Noman", 19, "Islamabad");
+
+console.log(Object.getPrototypeOf(Player.prototype) === Object.prototype);
+console.log(player1.valueOf());
+
+Object.prototype.saySmth = function(){//defining function on the object prototype, it is then inherited by all types of objects
+  console.log("What do I say?");
+}
+player1.saySmth()//shated by both object
+mustafa.saySmth()
+
+console.log(player1.hasOwnProperty('valueOf'));// false, because player1 does not has its own valueOf property
+console.log(Object.prototype.hasOwnProperty('valueOf')); // true, because Object has its own valueOf property
