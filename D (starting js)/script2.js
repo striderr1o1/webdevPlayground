fd69bf9f2bf4365744bb454b2ec691fb31c473f9
Ranger <mnipk1243@gmail.com>
2025-07-30 00:43:42 +0500

    new files

---
+++ D (starting js)/script2.js
@@ -0,0 +1,20 @@
+let animal = {
+  eats: true,
+  walk() {
+    alert("Animal walk");
+  }
+};
+
+let rabbit = {
+  jumps: true,
+  __proto__: animal
+};
+
+let longEar = {
+  earLength: 10,
+  __proto__: rabbit
+};
+
+// walk is taken from the prototype chain
+longEar.walk(); // Animal walk
+alert(longEar.jumps); // true (from rabbit)
