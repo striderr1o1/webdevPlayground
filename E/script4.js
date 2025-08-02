a59925e46adfc520b14589cc64122b1dfed4db37
Ranger <mnipk1243@gmail.com>
2025-08-03 03:36:09 +0500

    some comments addition

--- E/script4.js
+++ E/script4.js
@@ -1,6 +1,6 @@
-var age = 110;
+var age = 110;//let and const declarations are not attached to the window
 function showAge(){
-    let age = 100; //if i remove this, it will go one scope up and look for age.
+    let age = 100; //if i remove this, it will go one scope up and look for age. Right now, it will look for it here. 
     console.log(age);
 }
 
