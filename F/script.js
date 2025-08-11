961d20fab1b0a7ec3f50c70092fcf712cf7d2124
Ranger <mnipk1243@gmail.com>
2025-08-12 02:37:08 +0500

    more on classes

--- F/script.js
+++ F/script.js
@@ -8,3 +8,33 @@ let user = {
 }
 user.name = "Mustafa";
 console.log(user.name);
+//#####################################################
+class food {
+    constructor(name, calories) {
+        this.name = name;
+        this.calories = calories;
+    }
+
+    showFood(){
+        return {foodname: this.name, cal: this.calories};
+    }
+    
+}
+
+let spaghetti = new food("Spaghetti", 200);
+console.log(spaghetti.showFood()["foodname"]);
+
+class Person{
+    constructor(name, age, location){
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+    showPersonInfo(){
+        return {name: this.name, age: this.age, location: this.location};
+    }
+}
+
+let Ranger = new Person("Sikander", 20, "Karachi");
+
+console.log(Ranger.showPersonInfo());
