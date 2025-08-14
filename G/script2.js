aef9a8e85924454c5a49f037f0dd9b1f797ae4c1
Ranger <mnipk1243@gmail.com>
2025-08-15 02:24:24 +0500

    more on barrel.js

--- G/script2.js
+++ G/script2.js
@@ -1,7 +1,12 @@
-import {greeting2, saySomething, name1, name2} from "./script1.js";
+//importing stuff from barrel.js
+import {greeting2, saySomething, name1, name2} from "./barrel.js";
+// import defaultExport from "./script1.js";
 import { greet } from "./barrel.js";
 import { agee } from "./barrel.js";
 import { ShowActivities } from "./barrel.js";
 // //if it doesnt have a name, we can import it with any name
 // console.log(saySomething());
 // console.log(greeting + name1 + name2);
+
+console.log(greeting2());
+// console.log(defaultExport);
