2c13d7ba1b126e3ee1038f6050d839262548d3d1
Ranger <mnipk1243@gmail.com>
2025-08-16 22:26:54 +0500

    added comments in webconfig

--- H/webpack-practice/webpack.config.js
+++ H/webpack-practice/webpack.config.js
@@ -9,25 +9,31 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true
     },
+    //plugins
     plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
+  //loaders
    module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/i,//array should be written in reverse
         use: ["style-loader", "css-loader"],
       },
-      {
+      {//html loader for loading images in html
         test: /\.html$/i,
         loader: "html-loader",
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      }      
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },      
     ],
   },
   devtool: "eval-source-map",
